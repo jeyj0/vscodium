fb7fe26c079cf0024dc1d73109314a777e5de777
Jannis Jorre <jannis@jorre.dev>
2020-03-31 18:02:25 +0200

    use my own patched version of VSCode

--- get_repo.sh
+++ get_repo.sh
@@ -4,12 +4,13 @@ if [ -d vscode ]; then
   cd vscode 
   git fetch --all
 else
-  git clone https://github.com/Microsoft/vscode.git
+  git clone https://github.com/jeyj0/vscode.git
   cd vscode
 fi
 
 export LATEST_MS_COMMIT=$(git rev-list --tags --max-count=1)
 export LATEST_MS_TAG=$(git describe --tags ${LATEST_MS_COMMIT})
 echo "Got the latest MS tag: ${LATEST_MS_TAG}"
-git checkout $LATEST_MS_TAG
+# git checkout $LATEST_MS_TAG
+git checkout jeyj0-patches
 cd ..
