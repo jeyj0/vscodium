c48abb814950cb84a248da3ce8196b608a7142e5
Jannis Jorre <jannis@jorre.dev>
2020-03-31 18:07:17 +0200

    add committish to release name

--- get_repo.sh
+++ get_repo.sh
@@ -13,4 +13,5 @@ export LATEST_MS_TAG=$(git describe --tags ${LATEST_MS_COMMIT})
 echo "Got the latest MS tag: ${LATEST_MS_TAG}"
 # git checkout $LATEST_MS_TAG
 git checkout jeyj0-patches
+export COMMITTISH=$(git rev-parse --short HEAD)
 cd ..
